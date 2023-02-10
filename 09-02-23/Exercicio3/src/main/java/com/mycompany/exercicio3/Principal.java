c7dd89930b3b70718123e4bfb3967a8db63417bb
Pablo Santos <99838818+P4BLOll@users.noreply.github.com>
2023-02-09 23:08:01 -0300

    Exercícios de Java

--- 09-02-23/Exercicio3/src/main/java/com/mycompany/exercicio3/Principal.java
+++ 09-02-23/Exercicio3/src/main/java/com/mycompany/exercicio3/Principal.java
@@ -11,17 +11,19 @@ import java.util.Scanner;
  * @author dti
  */
 public class Principal {
-public static int numero;
-public static int resul;
+public static int numero = 0;
+public static int cont = 0;
     public static void main(String args[]){
       Scanner teclado = new Scanner(System.in);
-        for (int num = 0; num <= 5; num++) {
+        for (int num = 0; num <= 15; num++) {
           System.out.println("Digite um número: ");
-            numero = teclado.nextInt();
-            resul = numero - 30;
-            if(resul>0){
-            System.out.println(numero);
+            numero = teclado.nextInt();  
+            if (numero>30) {
+               cont++; 
             }
         }
+           System.out.println("");
+           System.out.println("A quantidade de números maiores que 30 são: "+cont);
+        
     }
 }
